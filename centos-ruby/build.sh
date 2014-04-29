e77b4a44d95054bef152271cf3a71ebbcc1dafaa
Michal Fojtik <mfojtik@redhat.com>
2014-04-30 00:01:18 +0200

    Added self-contained Ruby builder image

--- centos-ruby/build.sh
+++ centos-ruby/build.sh
@@ -2,3 +2,4 @@
 
 (cd base && docker build -t openshift/centos-ruby .)
 (cd extended && docker build -t openshift/centos-ruby-extended .)
+(cd builder && docker build -t openshift/centos-ruby-builder .)
